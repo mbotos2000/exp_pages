84ecd7ca9018fb1b6ba1b9db9887eb62d4c6a20f
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-02-18 16:41:14 +0200

    Update _5. Expertiză geotehnică exigența Af, privind stabilitatea amplasamentului.py

--- pages/_5. Expertiză geotehnică exigența Af, privind stabilitatea amplasamentului.py
+++ pages/_5. Expertiză geotehnică exigența Af, privind stabilitatea amplasamentului.py
@@ -198,9 +198,9 @@ if st.session_state['file']!=None or st.session_state['cond']!=None:
                 st.session_state.documente="\n".join(selected)
                 st.text_area('Denumire obiectiv pentru care se face expertiza', key='den_obiectiv')
                 try:
-                 st.text_area('Valoare expertiza geotehnica',value=str(format_eu_number(df.iloc[113, 8])), key='val_ET')
+                 aa=st.text_area('Valoare expertiza geotehnica',value=df.iloc[113, 8])
                 except:
-                 st.text_area('Valoare expertiza geotehnica', value=0.0, key='val_ET')                
+                 aa=st.text_area('Valoare expertiza geotehnica', value="0")                
                 colA, colB = st.columns(2)
                 with colA:
                  st.text_area('Numar ore necesar verificare',value="8",key='ore_et')
