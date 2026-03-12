653d018e1d2b3a6072cff54bcdb37e1e8b13a395
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-03-12 11:28:58 +0200

    Update _1.Expertiză servicii conexe.py

--- pages/_1.Expertiză servicii conexe.py
+++ pages/_1.Expertiză servicii conexe.py
@@ -254,14 +254,14 @@ if st.session_state['file']!=None or st.session_state['cond']!=None:
       st.session_state["total1"]=float(st.session_state["val_ET"].replace(".", "").replace(",", "."))+float(st.session_state["val_a_3d"].replace(".", "").replace(",", "."))+float(st.session_state["val_a_rel"].replace(".", "").replace(",", "."))+ float(st.session_state["val_inc_nd"].replace(".", "").replace(",", "."))+float(st.session_state["val_bet"].replace(".", "").replace(",", "."))+float(st.session_state["val_geo"].replace(".", "").replace(",", "."))+st.session_state["val_dezv_8"]
       #st.session_state["total2"]=float(st.session_state["val_et_finisaje"].replace(".", "").replace(",", "."))+float(st.session_state["val_rel_struct"].replace(".", "").replace(",", "."))+float(st.session_state["val_et_actualizat"].replace(".", "").replace(",", "."))
       st.session_state["total"]=st.session_state["total1"]#+st.session_state["total2"]
-      st.session_state["val_dezv_8"]=float_to_eu(st.session_state["val_dezv_8"])
-      st.session_state["total1"]=float_to_eu(st.session_state["total1"])
-      st.session_state["total12"]=float_to_eu(st.session_state["total12"])
+      st.session_state["val_dezv_8"]=format_number(str(st.session_state["val_dezv_8"]))
+      st.session_state["total1"]=format_number(str(st.session_state["total1"]))
+      st.session_state["total12"]=format_number(str(st.session_state["total12"]))
       #st.session_state["total2"]=float_to_eu(st.session_state["total2"])
-      st.session_state["total"]=float_to_eu(st.session_state["total"])
-      for key in ["val_inc_nd","val_ET","val_bet","val_bet_2","val_geo","val_dezveliri","val_a_3d","val_a_rel", "val_et_finisaje","val_rel_struct","val_et_actualizat", "tarif_et","val_rel","val_dezv_8"]:
+      st.session_state["total"]=format_number(str(st.session_state["total"]))
+      for key in ["val_inc_nd","val_ET","val_bet","val_bet_2","val_geo","val_dezveliri","val_a_3d","val_a_rel", "val_et_finisaje","val_rel_struct","val_et_actualizat", "tarif_et","val_rel",]:
        try:
-         st.session_state[key]=float_to_eu(float(st.session_state[key]))
+         st.session_state[key]=format_number(st.session_state[key])
        except:
          pass
       keys_to_merge=["val_inc_nd","val_ET","val_bet","val_bet_2","val_geo","val_dezveliri","val_a_3d","val_a_rel", "val_et_finisaje","val_rel_struct","val_et_actualizat",
